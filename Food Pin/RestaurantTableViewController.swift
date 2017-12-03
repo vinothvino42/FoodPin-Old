73de5297f16660fe0e67bdf33d82500d4555d94d
vinoth <vinothvino42@gmail.com>
2017-12-03 20:18:59 +0530

    Now you can CheckIn Restaurants and share it to social media

--- Food Pin/RestaurantTableViewController.swift
+++ Food Pin/RestaurantTableViewController.swift
@@ -24,6 +24,8 @@ class RestaurantTableViewController: UITableViewController {
     
     var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
     
+    var restaurantIsVisited = Array(repeating: false, count: 21)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -56,11 +58,16 @@ class RestaurantTableViewController: UITableViewController {
 
         // Configure the cell...
         cell.nameLabel?.text = restaurantNames[indexPath.row]
+        cell.locationLabel.text = restaurantLocations[indexPath.row]
+        cell.typeLabel.text = restaurantTypes[indexPath.row]
         cell.thumbnailImage?.image = UIImage(named: restaurantImages[indexPath.row])
         
         //To make circular image
         cell.thumbnailImage.layer.cornerRadius = 30
         cell.thumbnailImage.clipsToBounds = true
+        
+        //Checking checkIn
+        cell.accessoryType = restaurantIsVisited[indexPath.row] ? .checkmark : .none
         
         return cell
     }
@@ -69,28 +76,88 @@ class RestaurantTableViewController: UITableViewController {
         
         //Creating alert sheet
         let optionMenu = UIAlertController(title: nil, message: "What do you want to do ?", preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        optionMenu.addAction(cancelAction)
+
         //Handler for call action
         let callActionHandler = { (action: UIAlertAction!) -> Void in
-            
+
             let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry there's no call feature. Please try again later", preferredStyle: .alert)
             alertMessage.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
             self.present(alertMessage, animated: true, completion: nil)
         }
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        let callAction = UIAlertAction(title: "Call" + "8870595\(indexPath.row)" , style: .default, handler: callActionHandler)
-        let checkInAction = UIAlertAction(title: "Check In", style: .default, handler: {
-            
-            (action: UIAlertAction) -> Void in
-            
-            let cell = tableView.cellForRow(at: indexPath)
-            cell?.accessoryType = .checkmark
-        })
-        
+
+        let callAction = UIAlertAction(title: "Call " + "8870595\(indexPath.row)" , style: .default, handler: callActionHandler)
         optionMenu.addAction(callAction)
-        optionMenu.addAction(cancelAction)
+
+        let checkInTitle = restaurantIsVisited[indexPath.row] ? "Undo Check In" : "Check In"
+
+        let checkInAction = UIAlertAction(title: checkInTitle, style: .default, handler: {
+
+            (action: UIAlertAction) -> Void in
+
+            let cell = tableView.cellForRow(at: indexPath)
+
+            // Toggle check-in and undo-check-in
+            self.restaurantIsVisited[indexPath.row] = self.restaurantIsVisited[indexPath.row] ? false : true
+            cell?.accessoryType = self.restaurantIsVisited[indexPath.row] ? .checkmark : .none
+
+        })
+
         optionMenu.addAction(checkInAction)
-        
+
+        tableView.deselectRow(at: indexPath, animated: true)
         present(optionMenu, animated: true, completion: nil)
+        
+    }
+    
+//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+//
+//        if editingStyle == .delete {
+//
+//            //Removing all array values
+//            restaurantNames.remove(at: indexPath.row)
+//            restaurantLocations.remove(at: indexPath.row)
+//            restaurantTypes.remove(at: indexPath.row)
+//            restaurantIsVisited.remove(at: indexPath.row)
+//            restaurantImages.remove(at: indexPath.row)
+//
+//        }
+//       // tableView.reloadData()
+//        tableView.deleteRows(at: [indexPath], with: .fade)
+//    }
+    
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        
+        //Share Button
+        let shareAction = UITableViewRowAction(style: .default, title: "Share") { (action, indexPath) in
+            
+            let text = "Just check in at "+self.restaurantNames[indexPath.row]
+            if let imageToShare = UIImage(named: self.restaurantImages[indexPath.row]) {
+                let activityController = UIActivityViewController(activityItems: [text, imageToShare], applicationActivities: nil)
+                self.present(activityController, animated: true, completion: nil)
+            }
+        }
+        
+        //Delete Button
+        let deleteAction = UITableViewRowAction(style: .default, title: "Delete") { (action, indexPath) in
+            
+            //Delete the data
+            self.restaurantNames.remove(at: indexPath.row)
+            self.restaurantLocations.remove(at: indexPath.row)
+            self.restaurantTypes.remove(at: indexPath.row)
+            self.restaurantIsVisited.remove(at: indexPath.row)
+            self.restaurantImages.remove(at: indexPath.row)
+            
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+        
+        shareAction.backgroundColor = UIColor(red: 48.0/255.0, green: 173.0/255.0,
+                                             blue: 99.0/255.0, alpha: 1.0)
+        deleteAction.backgroundColor = UIColor(red: 202.0/255.0, green: 202.0/255.0,
+                                               blue: 203.0/255.0, alpha: 1.0)
+
+        
+        return [deleteAction,shareAction]
     }
 }
